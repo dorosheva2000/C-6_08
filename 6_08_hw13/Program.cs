6404740a5e47d100825da01947ab96a9a489ab5c
user <dorosheva2000@list.ru>
2023-08-20 05:21:49 +0300

    Второе задание

---
+++ 6_08_hw13/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+string TheThirdDigit(int num)
+{
+    if (num / 100 == 0) return "третьей цифры нет";
+    else if (num / 100 != 0 && num / 1000 == 0 ) return $"{num % 10}";
+    else
+    {
+        int bignum = num;
+        while (bignum > 999)
+        {
+            bignum = bignum / 10;
+        }
+        int res = bignum % 10;
+        return $"{res}";
+    }
+}
+
+int number = int.Parse(Console.ReadLine()!);
+Console.Write(TheThirdDigit(number));
