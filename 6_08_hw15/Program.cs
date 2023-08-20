8d9edde3e7787394b23bc39e577634aa14690312
user <dorosheva2000@list.ru>
2023-08-20 05:22:31 +0300

    Третье задание

---
+++ 6_08_hw15/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+string Weekend(int day)
+{
+    if(day == 6 || day == 7) return "да";
+    else return "нет";
+}
+
+int dow = int.Parse(Console.ReadLine()!);
+Console.Write(Weekend(dow));
