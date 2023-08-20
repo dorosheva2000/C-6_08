f99a0ee7ab34c1a32583641fcfd057f710f10e2e
user <dorosheva2000@list.ru>
2023-08-20 05:21:24 +0300

    Первое задание

---
+++ 6_08_hw10/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int ThreeDigit(int num)
+{
+    return num / 10 - num / 100 * 10;
+}
+
+int number = int.Parse(Console.ReadLine());
+Console.Write(ThreeDigit(number));
