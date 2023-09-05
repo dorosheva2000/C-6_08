175e6b355cf8766d4bd963014a736065b009d350
user <dorosheva2000@list.ru>
2023-09-05 19:21:28 +0300

    Вывод массива

---
+++ 27_08_hw29/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.Write("Please input array size: ");
+int S = int.Parse(Console.ReadLine()!);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    return array;
+}
+
+void FillArray(int[] array)
+{
+    Console.WriteLine("Please input arrae elements!");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);    
+    }
+}
+
+void Printarray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i ++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length-1] + "]");
+}
+
+int[] my_arr = CreateArray(S);
+FillArray(my_arr);
+Printarray(my_arr);
