8923b3ae646631731705c84e32ee324e251b1eb2
user <dorosheva2000@list.ru>
2023-09-05 19:20:58 +0300

    Сумма цифр в числе

---
+++ 27_08_hw27/Program.cs
@@ -0,0 +1,26 @@
+﻿
+/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int InputNumber(string mes)
+{
+    Console.Write(mes);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumOfDigits(int number)
+{
+    int sum = 0;
+    int num2 = number;
+    for (int i = 0; i < (int)Math.Log10(number) + 1; i ++)
+    {
+        sum += num2 % 10;
+        num2 = num2 / 10; 
+    }
+    return sum;
+}
+
+int N = InputNumber("Please input number: ");
+Console.Write(SumOfDigits(N));
